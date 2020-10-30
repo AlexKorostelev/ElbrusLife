ed979c453617838122631f79ff2c959b5ce9c9d0
HoneyMS <Honeyms@mail.ru>
2020-10-30 11:25:14 +0300

    Release new board

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -7,15 +7,17 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', async(req, res) => {
     const userName = res.locals.user;
-    const usersInListSelect = await User.find({ name: userName });
+		const usersInListSelect = await User.find();
+		const arrUsersFilter = usersInListSelect.filter(e=>e.name!=userName.name);
+		//Для таблицы
     const gameListForTableKikker = await GameList.find({ gameName: "Киккер" }).populate('idUserVinner').populate('idUser2');
     const gameListForTennis = await GameList.find({ gameName: "Настольный теннис" }).populate('idUserVinner').populate('idUser2');
-    res.render('leaderboard', { userName, usersInListSelect, gameListForTableKikker, gameListForTennis });
+    res.render('leaderboard', { userName, arrUsersFilter, gameListForTableKikker, gameListForTennis });
 });
 
 router.post('/', async(req, res) => {
     const { fioVinner, fioOpponent, gameName, gameRank } = req.body
-    console.log(req.body);
+    // console.log(req.body);
     const newGame = new GameList({
         gameName: gameName,
         idUserVinner: fioVinner,
